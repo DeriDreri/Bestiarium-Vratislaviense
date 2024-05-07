37aa8b04200d535eef3ec2bb90b93b08f9ac48f5
DeriDreri <dominikcybulski@protonmail.com>
2024-05-07 18:02:36 +0200

    Add searchbar for list of creatures

--- website/src/CreaturesList.js
+++ website/src/CreaturesList.js
@@ -2,6 +2,10 @@ import Creature from "./Creature.js"
 const CreaturesList = ({isMapView, creatures}) => {
   return (
     <div className="creatures-list">
+      <form> 
+        <input type="text"/>
+        <button>Szukaj</button>
+      </form>
       {
         creatures.map(creature => (
         <Creature isMapView={true} name={creature.name} location={creature.location} historiesAmount={creature.historiesAmount} />
